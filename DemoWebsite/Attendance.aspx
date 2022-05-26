07851d86a06cab4516e3122aa2335948c8aa2058
Fahad Waseem <fahadwaseem8@gmail.com>
2022-05-26 21:13:33 +0500

    Added attendance page frontend and connectivity. Only need to add queries now

--- DemoWebsite/Attendance.aspx
+++ DemoWebsite/Attendance.aspx
@@ -1,10 +1,85 @@
 ﻿<%@ Page Title="Attendance" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Attendance.aspx.cs" Inherits="DemoWebsite.Attendance" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <style type="text/css">
+        .style1
+        {
+            width: 37%;
+            background-color: lightcyan;
+        }
+    </style>
 
-    <h2><%: Title %>.</h2>
-    <br>
-    <p>Form here</p>
-
-
+    <div>
+ 
+        <h1 >
+            Mark Attendance</h1>
+     
+ 
+    </div>
+ 
+    <br />
+    <br />
+ 
+    <p>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </p>
+    <table border="2" class="style1">
+        <tr>
+            <td>
+                Select Class :
+            </td>
+            <td>
+                <asp:DropDownList ID="DDListClass" runat="server"
+                    AutoPostBack="true"
+                    onselectedindexchanged="DropDownList1_SelectedIndexChanged">
+                    <asp:ListItem Text="Programming Fundamentals" Value="CS"></asp:ListItem>
+                    <asp:ListItem Text="Calculus 1" Value="CS"></asp:ListItem>
+                    <asp:ListItem Text="Physics" Value="CS"></asp:ListItem>
+                    <asp:ListItem Text="Information Technology" Value="CS"></asp:ListItem>
+                    <asp:ListItem Text="Pakistan Studies" Value="CS"></asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>
+ 
+    <br />
+ 
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+        BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px"
+        CellPadding="3" CellSpacing="2" >
+        <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+        <Columns>
+            <asp:BoundField DataField="rollno" HeaderText="Roll No."
+                SortExpression="rollno" />
+            <asp:BoundField DataField="studentname" HeaderText="Student Name"
+                SortExpression="studentname" />
+            <asp:TemplateField HeaderText="Mark Attendance">
+                <ItemTemplate>
+                    <asp:RadioButton ID="RadioButton1" runat="server" Checked="True" GroupName="G1"
+                        Text="Present" />
+                    &nbsp;<asp:RadioButton ID="RadioButton2" runat="server" GroupName="G1"
+                        Text="Absent" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+        <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+        <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+    </asp:GridView>
+    <br />
+    <asp:Button ID="BtnSave" runat="server"
+        Text="Save Attendance" onclick="BtnSave_Click" />
+    <br />
+    <br />
+    <asp:Label ID="LblMsg" runat="server" Text="Label"></asp:Label>
+    <br />
+    <br />
+    <br />
 
 </asp:Content>
--- DemoWebsite/Attendance.aspx.cs
+++ DemoWebsite/Attendance.aspx.cs
@@ -1,17 +1,167 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
 
+
+// TODO add queries according to table
 namespace DemoWebsite
 {
     public partial class Attendance : System.Web.UI.Page
     {
+        private static readonly string connString = System.Configuration.ConfigurationManager.ConnectionStrings["SQLDbConnection"].ConnectionString;
+
+        SqlConnection Cn;
+        SqlDataAdapter Da;
+        DataSet Ds;
+        int Max = 0;
+        string str;
         protected void Page_Load(object sender, EventArgs e)
         {
+            LblMsg.Visible = false;
+            Label1.Text = "Mark Attendance for " + DateTime.Now.ToShortDateString();
+            if (!IsPostBack)
+            {
+                Class_Onload();
+            }
+        }
 
+
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection Cn = new SqlConnection(connString))
+            {
+                try
+                {
+                    Cn.Open();
+                    Da = new SqlDataAdapter("SELECT * FROM enrollment WHERE courseid='" + DDListClass.SelectedItem.Text.Trim() + "'", Cn);
+
+
+                    Ds = new DataSet();
+                    Da.Fill(Ds, "TempStud");
+                    Max = Ds.Tables["TempStud"].Rows.Count;
+
+                    if (Max > 0)
+                    {
+
+                        GridView1.DataSource = Ds.Tables["TempStud"];
+
+                        GridView1.DataBind();
+                        Cn.Close();
+
+                    }
+                    else
+                    {
+                        Ds.Tables["TempStud"].Rows.Add(Ds.Tables["TempStud"].NewRow());
+
+                        GridView1.DataSource = Ds;
+                        GridView1.DataBind();
+
+                        int GViewColumnCount = GridView1.Rows[0].Cells.Count;
+                        GridView1.Rows[0].Cells.Clear();
+                        GridView1.Rows[0].Cells.Add(new TableCell());
+                        GridView1.Rows[0].Cells[0].ColumnSpan = GViewColumnCount;
+
+                        GridView1.Rows[0].Cells[0].Text = "No Records Found.";
+
+                    }
+
+                    Cn.Close();
+                }
+                catch
+                {
+                    Response.Write("<script language=javascript>alert('Database connection not found.')</script>");
+                    return;
+                }
+
+
+            }
+        }
+
+        public void Class_Onload()
+        {
+            using (SqlConnection Cn = new SqlConnection(connString))
+            {
+                try
+                {
+                    Cn.Open();
+                    Da = new SqlDataAdapter("SELECT * FROM StudentDetails", Cn);
+                    Ds = new DataSet();
+                    Da.Fill(Ds, "TempClass");
+                    Max = Ds.Tables["TempClass"].Rows.Count;
+
+                    if (Max > 0)
+                    {
+                        DDListClass.DataTextField = Ds.Tables["TempClass"].Columns["class"].ToString(); // text field name of table dispalyed in dropdown
+
+                        DDListClass.DataSource = Ds.Tables["TempClass"];      //assigning Value datasource to the dropdownlist
+                        DDListClass.DataBind();  //binding dropdownlist
+                        DDListClass.Items.Insert(0, new ListItem("--Select Class --", "0"));
+                        Cn.Close();
+
+                    }
+                    else
+                    {
+                        DDListClass.DataSource = Ds.Tables["TempClass"];      //assigning Value datasource to the dropdownlist
+                        DDListClass.DataBind();  //binding dropdownlist
+                        DDListClass.Items.Insert(0, new ListItem("--Class not found--", "0"));
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection Cn = new SqlConnection(connString))
+            {
+                Cn.Open();
+                int rollno = 0;
+                String studentname = "", dateofclass = "", sclass = "", status = "";
+
+                foreach (GridViewRow row in GridView1.Rows)
+                {
+
+                    rollno = Convert.ToInt32(row.Cells[0].Text);
+                    studentname = row.Cells[1].Text;
+                    RadioButton rbtn1 = (row.Cells[2].FindControl("RadioButton1") as RadioButton);
+                    RadioButton rbtn2 = (row.Cells[2].FindControl("RadioButton2") as RadioButton);
+
+                    if (rbtn1.Checked)
+                    {
+                        status = "Present";
+
+                    }
+                    else
+                    {
+                        status = "Absent";
+                    }
+                    dateofclass = DateTime.Now.ToShortDateString();
+                    sclass = DDListClass.SelectedItem.Text;
+
+                }
+
+
+
+                str = "INSERT INTO Attendance(rollno , studentname , dateofclass , attendancestatus, class ) VALUES('" + rollno + "','" + studentname + "','" + dateofclass + "','" + status + "','" + sclass + "')";
+                //int x = Connection.UpdateRecord(Cn, str);
+                Cn.Close();
+                LblMsg.Visible = true;
+                LblMsg.Text = "Attendance Has Been Saved Successfully";
+
+            }
         }
     }
 }
