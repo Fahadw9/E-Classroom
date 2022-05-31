d856e8a123a5dd3915c9eb78d054ffb70bf8bf2c
art <ali.hussnainbabar@gmail.com>
2022-06-01 01:52:52 +0500

    Added "Information Retrieval" page with Grade query funtionality

---
+++ DemoWebsite/Query.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="Query" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Query.aspx.cs" Inherits="DemoWebsite.Query" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <p>Grading</p>
+      <style>
+        .list{ border-radius: 5px;
+                  border: 2px solid #0000FF;
+                box-shadow: 0 0 5px #0000FF;
+          }
+    </style>
+    <p>
+    <br>
+  <asp:Label ID="labelId" runat="server">Max Grade</asp:Label>
+            <asp:TextBox ID="G1" runat="server" ToolTip="Enter the upper grade range"></asp:TextBox>
+            <br><br>
+            <asp:Label ID="label3" runat="server">Min Grade</asp:Label>
+            <asp:TextBox ID="G2" runat="server" ToolTip="Enter the lower grade range"></asp:TextBox>
+    &nbsp;&nbsp;
+        <br />
+        <br />        
+    <asp:DropDownList ID="list1" runat="server" CssClass ="list">
+    <asp:ListItem Text="Select Course" Value ="0"></asp:ListItem>
+    <asp:ListItem Text="Programming Fundamentals" Value = "1"></asp:ListItem>
+    <asp:ListItem Text="Calculus 1" Value="2"></asp:ListItem>
+    <asp:ListItem Text="Physics" Value ="3"></asp:ListItem>
+    <asp:ListItem Text="Pakistan Studies" Value ="4"></asp:ListItem>
+    <asp:ListItem Text="ICT" Value="5"></asp:ListItem>
+    </asp:DropDownList>
+    &nbsp;&nbsp; 
+    <asp:Button ID="btnSave" Text="Enter" CssClass="roundbutton" runat="server" OnClick="btn_Grade_Click" />
+    </p>
+    <br />
+     <asp:GridView ID="GridView3" runat="server" AutoGenerateColumns="False" 
+        BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px"
+        CellPadding="3" CellSpacing="2" >
+        <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+         <Columns>
+              <asp:BoundField DataField="Email" HeaderText="Email"
+                 />
+              <asp:BoundField DataField="Course" HeaderText="Course"
+                 />
+            <asp:BoundField DataField="Grade" HeaderText="Grade"
+                 />
+             </Columns>
+        <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+        <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+        <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+    </asp:GridView>
+</asp:Content>
---
+++ DemoWebsite/Query.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace DemoWebsite
+{
+    public partial class Query : System.Web.UI.Page
+    {
+        private static readonly string connString = System.Configuration.ConfigurationManager.ConnectionStrings["SQLDbConnection"].ConnectionString;
+
+        SqlConnection Cn;
+        SqlDataAdapter Da;
+        DataSet Ds;
+        int Max = 0;
+        string str;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Grade_Click(object sender, EventArgs e)
+        { 
+            {
+                using (SqlConnection Cn = new SqlConnection(connString))
+                {
+                    Cn.Open();
+                    String MinRange;
+                    String MaxRange;
+                    try
+                    {
+                        if (Session["usertype"].ToString() == "teacher")
+                        {
+
+                            if (G1.Text.ToString() == "")
+                            {
+                                MaxRange = "A";
+                            }
+                            else
+                            {
+                                MaxRange = G1.Text.ToString();
+                            }
+                            if (G2.Text.ToString() == "")
+                            {
+                                MinRange = "F";
+                            }
+                            else
+                            {
+                                MinRange = G2.Text.ToString();
+                            }
+                            if ((G1.Text.ToString() != "A" && G1.Text.ToString() != "B" && G1.Text.ToString() != "C" && G1.Text.ToString() != "D" && G1.Text.ToString() != "E" && G1.Text.ToString() != "F") ||
+                                (G2.Text.ToString() != "A" && G2.Text.ToString() != "B" && G2.Text.ToString() != "C" && G2.Text.ToString() != "D" && G2.Text.ToString() != "E" && G2.Text.ToString() != "F"))
+                            {
+                                Response.Write("<script language=javascript>alert('Invalid Range For Grades Entered.')</script>");
+                                return;
+                            }
+
+
+                            DataTable dt = new DataTable();
+
+                            DataColumn dc = new DataColumn("Email", typeof(String));
+                            dt.Columns.Add(dc);
+                            dc = new DataColumn("Course", typeof(String));
+                            dt.Columns.Add(dc);
+                            dc = new DataColumn("Grade", typeof(String));
+                            dt.Columns.Add(dc);
+                            DataRow dr = dt.NewRow();
+                            String query;
+                            if (list1.SelectedValue.ToString() == "0")
+                            {
+                                query = ("select * from grade where lettergrade >= '" + MaxRange + "' AND  lettergrade <= '" + MinRange + "'");
+                            }
+                            else
+                            {
+                                query = ("select * from grade where lettergrade >= '" + MaxRange + "' AND  lettergrade <= '" + MinRange + "' AND course = '" + list1.SelectedItem.Text.ToString() + "'");
+                            }
+                            SqlCommand cmd = new SqlCommand(query, Cn);
+                            SqlDataReader reader = cmd.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                dr[0] = reader[0].ToString();
+                                dr[1] = reader[1].ToString();
+                                dr[2] = reader[2].ToString();
+                                dt.Rows.Add(dr);
+                                dr = dt.NewRow();
+                            }
+                            GridView3.DataSource = dt;
+                            GridView3.DataBind();
+
+                            Cn.Close();
+
+                           
+                        }
+                    }
+                      catch
+                             {
+                                 Response.Write("<script language=javascript>alert('Database connection not found.')</script>");
+                                 return;
+                             }
+
+                }
+
+            }
+        }
+    }
+}
